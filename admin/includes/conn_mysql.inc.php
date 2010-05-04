6a7c91e00273fd6a398d3ac1dd0d49b393d7151f
Jim Wharton <jamesawharton@gmail.com>
2010-05-04 17:13:26 -0400

    changed connect for atlantic.net

--- admin/includes/conn_mysql.inc.php
+++ admin/includes/conn_mysql.inc.php
@@ -1,16 +1,19 @@
 <?php
 function dbConnect($type) {
   if ($type == 'query') {
-    	$user = 'sb_view';
-		$pwd = 'viewonly';
+    	//$user = 'sb_view';
+		//$pwd = 'viewonly';
+		$user = 'sbwd';
+		$pwd = 'ur3133t';
 	} 
 	elseif ($type == 'admin') {
-		$user = 'sb_admin';
+		//$user = 'sb_admin';
+		$user = 'sbwd';
 		$pwd = 'ur3133t';
 	} else {
 		exit('Unrecognized connection type');
 	}
-  $conn = mysql_connect('localhost', $user, $pwd) or die ('Cannot connect to server');
+  $conn = mysql_connect('mysql1.atlantic.net', $user, $pwd) or die ('Cannot connect to server');
   mysql_select_db('sbwd') or die ('Cannot open database');
   return $conn;
   }
