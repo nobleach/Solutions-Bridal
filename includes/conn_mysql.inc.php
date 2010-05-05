8f4804f30f4bc23b8ba9e86e7ea58057b3a1175d
Jim Wharton <jamesawharton@gmail.com>
2010-05-05 08:04:54 -0400

    about to be overwritten

--- includes/conn_mysql.inc.php
+++ includes/conn_mysql.inc.php
@@ -1,11 +1,14 @@
 <?php
 function dbConnect($type) {
   if ($type == 'query') {
-    	$user = 'sb_view';
-		$pwd = 'viewonly';
+    	//$user = 'sb_view';
+		//$pwd = 'viewonly';
+		$user = 'sbwd@localhost';
+		$pwd = 'ur3133t';
 	} 
 	elseif ($type == 'admin') {
-		$user = 'sb_admin';
+		//$user = 'sb_admin';
+		$user = 'sbwd@localhost';
 		$pwd = 'ur3133t';
 	} else {
 		exit('Unrecognized connection type');
